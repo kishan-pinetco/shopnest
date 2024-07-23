7b82e1d61496836cc92c4c171be3cd97ca6e9df8
vishvjit2210 <148254408+vishvjit2210@users.noreply.github.com>
2024-07-23 20:36:51 +0530

    work_in_progress

--- authentication/user_auth/login.php
+++ authentication/user_auth/login.php
@@ -21,7 +21,7 @@
     <link href="https://fonts.googleapis.com/css2?family=Outfit:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
 
     <!-- favicon -->
-    <link rel="shortcut icon" href="../src/logo/favicon.svg">
+    <link rel="shortcut icon" href="/src/logo/favicon.svg">
 
 </head>
 
