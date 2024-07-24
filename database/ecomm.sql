933c353a0f74708de8755b539925edb37327111f
abhijeetdabhi <abhijeetdabhi3904@gmail.com>
2024-07-24 14:36:57 +0530

    change directorys of sql file and connect file

---
+++ database/ecomm.sql
@@ -0,0 +1,287 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jul 24, 2024 at 10:53 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `ecomm`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `cancel_orders`
+--
+
+CREATE TABLE `cancel_orders` (
+  `cancel_order_id` int(11) NOT NULL,
+  `order_id` varchar(255) DEFAULT NULL,
+  `product_id` varchar(255) DEFAULT NULL,
+  `user_id` varchar(255) DEFAULT NULL,
+  `vendor_id` varchar(255) DEFAULT NULL,
+  `user_name` varchar(255) DEFAULT NULL,
+  `user_email` varchar(255) DEFAULT NULL,
+  `user_phone` varchar(255) DEFAULT NULL,
+  `cancel_order_image` varchar(255) DEFAULT NULL,
+  `cancel_order_tital` varchar(255) DEFAULT NULL,
+  `billing_email` varchar(255) DEFAULT NULL,
+  `reason` varchar(255) DEFAULT NULL,
+  `date` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `orders`
+--
+
+CREATE TABLE `orders` (
+  `order_id` int(11) NOT NULL,
+  `order_tital` varchar(255) DEFAULT NULL,
+  `order_image` varchar(255) DEFAULT NULL,
+  `order_price` varchar(255) DEFAULT NULL,
+  `user_id` varchar(255) DEFAULT NULL,
+  `product_id` varchar(255) DEFAULT NULL,
+  `vendor_id` varchar(255) DEFAULT NULL,
+  `user_first_name` varchar(255) DEFAULT NULL,
+  `user_last_name` varchar(255) DEFAULT NULL,
+  `user_email` varchar(255) DEFAULT NULL,
+  `user_mobile` varchar(255) DEFAULT NULL,
+  `user_address` varchar(255) DEFAULT NULL,
+  `user_state` varchar(255) DEFAULT NULL,
+  `user_city` varchar(255) DEFAULT NULL,
+  `user_pin` varchar(255) DEFAULT NULL,
+  `payment_type` varchar(255) DEFAULT NULL,
+  `status` varchar(255) DEFAULT NULL,
+  `total_price` varchar(255) DEFAULT NULL,
+  `vendor_profit` varchar(255) DEFAULT NULL,
+  `admin_profit` varchar(255) DEFAULT NULL,
+  `date` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `products`
+--
+
+CREATE TABLE `products` (
+  `product_id` int(11) NOT NULL,
+  `vendor_id` int(11) DEFAULT NULL,
+  `tital` varchar(255) DEFAULT NULL,
+  `image_1` varchar(255) DEFAULT NULL,
+  `image_2` varchar(255) DEFAULT NULL,
+  `image_3` varchar(255) DEFAULT NULL,
+  `image_4` varchar(255) DEFAULT NULL,
+  `cover_image_1` varchar(255) DEFAULT NULL,
+  `cover_image_2` varchar(255) DEFAULT NULL,
+  `cover_image_3` varchar(255) DEFAULT NULL,
+  `cover_image_4` varchar(255) DEFAULT NULL,
+  `company_name` varchar(255) DEFAULT NULL,
+  `Category` varchar(255) DEFAULT NULL,
+  `Type` varchar(255) DEFAULT NULL,
+  `Your_Price` varchar(255) DEFAULT NULL,
+  `MRP` varchar(255) DEFAULT NULL,
+  `Quantity` varchar(255) DEFAULT NULL,
+  `Item_Condition` varchar(255) DEFAULT NULL,
+  `Description` varchar(255) DEFAULT NULL,
+  `color` varchar(255) DEFAULT NULL,
+  `size` varchar(255) DEFAULT NULL,
+  `date` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `return_orders`
+--
+
+CREATE TABLE `return_orders` (
+  `return_order_id` int(11) NOT NULL,
+  `order_id` varchar(255) DEFAULT NULL,
+  `product_id` varchar(255) DEFAULT NULL,
+  `user_id` varchar(255) DEFAULT NULL,
+  `vendor_id` varchar(255) DEFAULT NULL,
+  `user_name` varchar(255) DEFAULT NULL,
+  `user_email` varchar(255) DEFAULT NULL,
+  `user_phone` varchar(255) DEFAULT NULL,
+  `return_order_image` varchar(255) DEFAULT NULL,
+  `return_order_tital` varchar(255) DEFAULT NULL,
+  `billing_email` varchar(255) DEFAULT NULL,
+  `reason` varchar(255) DEFAULT NULL,
+  `date` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_registration`
+--
+
+CREATE TABLE `user_registration` (
+  `user_id` int(11) NOT NULL,
+  `first_name` varchar(255) DEFAULT NULL,
+  `last_name` varchar(255) DEFAULT NULL,
+  `phone` varchar(255) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `profile_image` varchar(255) DEFAULT NULL,
+  `Address` varchar(255) DEFAULT NULL,
+  `state` varchar(255) DEFAULT NULL,
+  `city` varchar(255) DEFAULT NULL,
+  `pin` varchar(255) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `date` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `user_registration`
+--
+
+INSERT INTO `user_registration` (`user_id`, `first_name`, `last_name`, `phone`, `email`, `profile_image`, `Address`, `state`, `city`, `pin`, `password`, `date`) VALUES
+(2, 'Abhijeet', 'Dabhi', '7863843776', 'abhijeet@gmail.com', '1716458838240.jpg', '103,Madhav flat, Gajanand park soc, bharimata road ,cozway road, surat', 'Gujarat', 'Surat', '395004', '$2y$10$WV9A3CFJr0M/tNI59NxHt.pQoPP0bhguTZOFw8/MY4Kqm14R/YRAi', '24-07-2024');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_review`
+--
+
+CREATE TABLE `user_review` (
+  `review_id` int(11) NOT NULL,
+  `product_id` varchar(255) DEFAULT NULL,
+  `user_id` varchar(255) DEFAULT NULL,
+  `Rating` varchar(255) DEFAULT NULL,
+  `Review` varchar(255) DEFAULT NULL,
+  `public_name` varchar(255) DEFAULT NULL,
+  `date` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `vendor_registration`
+--
+
+CREATE TABLE `vendor_registration` (
+  `vendor_id` int(11) NOT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `phone` varchar(255) DEFAULT NULL,
+  `Bio` varchar(255) DEFAULT NULL,
+  `GTS` varchar(255) DEFAULT NULL,
+  `cover_image` varchar(255) DEFAULT NULL,
+  `dp_image` varchar(255) DEFAULT NULL,
+  `date` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `cancel_orders`
+--
+ALTER TABLE `cancel_orders`
+  ADD PRIMARY KEY (`cancel_order_id`);
+
+--
+-- Indexes for table `orders`
+--
+ALTER TABLE `orders`
+  ADD PRIMARY KEY (`order_id`);
+
+--
+-- Indexes for table `products`
+--
+ALTER TABLE `products`
+  ADD PRIMARY KEY (`product_id`);
+
+--
+-- Indexes for table `return_orders`
+--
+ALTER TABLE `return_orders`
+  ADD PRIMARY KEY (`return_order_id`);
+
+--
+-- Indexes for table `user_registration`
+--
+ALTER TABLE `user_registration`
+  ADD PRIMARY KEY (`user_id`);
+
+--
+-- Indexes for table `user_review`
+--
+ALTER TABLE `user_review`
+  ADD PRIMARY KEY (`review_id`);
+
+--
+-- Indexes for table `vendor_registration`
+--
+ALTER TABLE `vendor_registration`
+  ADD PRIMARY KEY (`vendor_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `cancel_orders`
+--
+ALTER TABLE `cancel_orders`
+  MODIFY `cancel_order_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `orders`
+--
+ALTER TABLE `orders`
+  MODIFY `order_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `products`
+--
+ALTER TABLE `products`
+  MODIFY `product_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `return_orders`
+--
+ALTER TABLE `return_orders`
+  MODIFY `return_order_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `user_registration`
+--
+ALTER TABLE `user_registration`
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `user_review`
+--
+ALTER TABLE `user_review`
+  MODIFY `review_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `vendor_registration`
+--
+ALTER TABLE `vendor_registration`
+  MODIFY `vendor_id` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
