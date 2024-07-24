c6cd9cd657be3e838d92b286c2e443b1d924dc97
vishvjit2210 <148254408+vishvjit2210@users.noreply.github.com>
2024-07-24 14:19:50 +0530

    work_in_progress

--- authentication/user_auth/user_register.php
+++ authentication/user_auth/user_register.php
@@ -1,11 +1,10 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-    <!-- css file link -->
-    <link rel="stylesheet" href="authentication.css">
+    <title>User Registration</title>
 
     <!-- Tailwind Script  -->
     <script src="https://cdn.tailwindcss.com?plugins=forms,typography,aspect-ratio,line-clamp"></script>
@@ -20,12 +19,9 @@
 
     <!-- alpinejs CDN -->
     <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
-
-    <!-- title -->
-    <title>User Registration</title>
 </head>
 
-<body class="flex justify-center h-[100%] p-2">
+<body class="flex justify-center h-[100%] p-2 outfit">
     <div class="lg:w-[45%]">
         <!-- header -->
         <div class="p-2 flex items-center justify-center">
@@ -48,13 +44,13 @@
                 </svg>
             </div>
         </div>
-        <form enctype="multipart/form-data" action="" method="post">
+        <form action="">
             <div class="border-2 rounded-md">
                 <h1 class="border-b-2 p-2 text-2xl font-semibold">User Registration</h1>
                 <!-- Profile Picture -->
                 <div class="w-full flex flex-col items-center relative mt-3">
                     <img id="previewImage" class="w-16 h-16 rounded-full border object-cover object-center border-black" alt="" src="https://w7.pngwing.com/pngs/178/595/png-transparent-user-profile-computer-icons-login-user-avatars-thumbnail.png">
-                    <input class="hidden" type="file" id="imageInput" name="UserImage">
+                    <input class="hidden" type="file" id="imageInput">
                     <label for="imageInput" class="absolute bottom-0 translate-y-3 translate-x-[2px] rounded-full bg-white p-1"><svg xmlns="http://www.w3.org/2000/svg" version="1.1" xmlns:xlink="http://www.w3.org/1999/xlink" width="16" height="16" x="0" y="0" viewBox="0 0 24 24" style="enable-background:new 0 0 512 512" xml:space="preserve">
                             <g>
                                 <g data-name="Layer 53">
@@ -84,18 +80,20 @@
                     <div class="flex flex-col gap-1 ">
                         <label for="fname" class="require font-semibold">First Name :</label>
                         <input class="h-12 rounded-md border-2 border-gray-300 hover:border-indigo-500 hover:transition" type="text" name="fname" id="fname">
-                        <small id="FnameValid" class="text-red-400 hidden">name must be 2-10 character long and shuld not start with a number</small>
+                        <p class="hidden text-sm font-medium translate-x-1 text-red-600">Please enter first name !</p>
+                        <!-- remove hidden class to show in <p> tag -->
                     </div>
                     <div class="flex flex-col gap-1 ">
-                        <label for="lname" class="require font-semibold">Last Name :</label>
-                        <input class="h-12 rounded-md border-2 border-gray-300 hover:border-indigo-500 hover:transition" type="text" name="lname" id="lname">
-                        <small id="LnameValid" class="text-red-400 hidden">name must be 2-10 character long and shuld not start with a number</small>
+                        <label for="name" class="require font-semibold">Last Name :</label>
+                        <input class="h-12 rounded-md border-2 border-gray-300 hover:border-indigo-500 hover:transition" type="text" name="name" id="name">
+                        <p class="hidden text-sm font-medium translate-x-1 text-red-600">Please enter last name !</p>
+                        <!-- remove hidden class to show in <p> tag -->
                     </div>
                     <div class="flex flex-col gap-1">
                         <label for="email" class="require font-semibold">Email :</label>
                         <input class="h-12 rounded-md border-2 border-gray-300 hover:border-indigo-500 hover:transition" type="email" name="email" id="email">
-                        <small id="MailValid" class="text-red-400 hidden">Enter Valid Email</small>
-                        
+                        <p class="hidden text-sm font-medium translate-x-1 text-red-600">Please enter email !</p>
+                        <!-- remove hidden class to show in <p> tag -->
                     </div>
                     <div class="flex flex-col gap-1 relative" x-data="{ showPassword: false }">
                         <label for="password" class="require font-semibold">Password :</label>
@@ -104,36 +102,42 @@
                                 <path d="M12 9a3.02 3.02 0 0 0-3 3c0 1.642 1.358 3 3 3 1.641 0 3-1.358 3-3 0-1.641-1.359-3-3-3z"></path>
                                 <path d="M12 5c-7.633 0-9.927 6.617-9.948 6.684L1.946 12l.105.316C2.073 12.383 4.367 19 12 19s9.927-6.617 9.948-6.684l.106-.316-.105-.316C21.927 11.617 19.633 5 12 5zm0 12c-5.351 0-7.424-3.846-7.926-5C4.578 10.842 6.652 7 12 7c5.351 0 7.424 3.846 7.926 5-.504 1.158-2.578 5-7.926 5z"></path>
                             </svg></span>
-                        <small id="passValid" class="text-red-400 hidden">password contains At least 8 digits</small>
+                        <p class="hidden text-sm font-medium translate-x-1 text-red-600">Please enter password !</p>
+                        <!-- remove hidden class to show in <p> tag -->
                     </div>
                     <div class="flex flex-col gap-1 md:col-span-2">
-                        <label for="address" class="require font-semibold">Address :</label>
+                        <label for="username" class="require font-semibold">Address :</label>
                         <textarea class="h-full rounded-md border-2 border-gray-300 hover:border-indigo-500 hover:transition resize-none" name="address" id="address"></textarea>
-                        <small id="addressValid" class="text-red-400 hidden">Enter Valid Address</small>
+                        <p class="hidden text-sm font-medium translate-x-1 text-red-600">Please enter address !</p>
+                        <!-- remove hidden class to show in <p> tag -->
                     </div>
                     <div class="flex flex-col gap-1">
-                        <label for="mobileno" class="require font-semibold">Mobile No :</label>
+                        <label for="username" class="require font-semibold">Mobile No :</label>
                         <input class="h-12 rounded-md border-2 border-gray-300 hover:border-indigo-500 hover:transition" type="tel" name="mobileno" id="mobileno" maxlength="10">
-                        <small id="mobilenoValid" class="text-red-400 hidden">Enter Valid Numbers</small>
+                        <p class="hidden text-sm font-medium translate-x-1 text-red-600">Please enter mobile number !</p>
+                        <!-- remove hidden class to show in <p> tag -->
                     </div>
                     <div class="flex flex-col gap-1">
-                        <label for="state" class="require font-semibold">State :</label>
+                        <label for="username" class="require font-semibold">State :</label>
                         <input class="h-12 rounded-md border-2 border-gray-300 hover:border-indigo-500 hover:transition" type="text" name="state" id="state">
-                        <small id="stateValid" class="text-red-400 hidden">Enter Valid State</small>
+                        <p class="hidden text-sm font-medium translate-x-1 text-red-600">Please enter state !</p>
+                        <!-- remove hidden class to show in <p> tag -->
                     </div>
                     <div class="flex flex-col gap-1">
-                        <label for="city" class="require font-semibold">City :</label>
+                        <label for="username" class="require font-semibold">City :</label>
                         <input class="h-12 rounded-md border-2 border-gray-300 hover:border-indigo-500 hover:transition" type="text" name="city" id="city">
-                        <small id="cityValid" class="text-red-400 hidden">Enter Valid City</small>
+                        <p class="hidden text-sm font-medium translate-x-1 text-red-600">Please enter city !</p>
+                        <!-- remove hidden class to show in <p> tag -->
                     </div>
                     <div class="flex flex-col gap-1">
-                        <label for="pincode" class="require font-semibold">Pincode :</label>
+                        <label for="username" class="require font-semibold">Pincode :</label>
                         <input class="h-12 rounded-md border-2 border-gray-300 hover:border-indigo-500 hover:transition" type="tel" name="pincode" id="pincode" maxlength="6">
-                        <small id="pincodeValid" class="text-red-400 hidden">Enter Valid Pincode</small>
+                        <p class="hidden text-sm font-medium translate-x-1 text-red-600">Please enter Pincode !</p>
+                        <!-- remove hidden class to show in <p> tag -->
                     </div>
                 </div>
                 <div class="flex justify-center mb-5">
-                    <input type="submit" value="Register" name="regBtn" class="bg-indigo-600 font-semibold h-10 w-72 text-lg rounded-md text-white cursor-pointer hover:bg-indigo-700 hover:transition">
+                    <button class="bg-indigo-500 font-semibold h-10 w-72 text-lg rounded-md text-white hover:bg-indigo-600 hover:transition">Register</button>
                 </div>
             </div>
         </form>
@@ -142,206 +146,6 @@
             <a class="underline font-semibold" href="user_login.php">Already a member? Login</a>
         </div>
     </div>
-
-    <!-- Error message container -->
-    <div class="validInfo fixed top-0 mt-2 w-full transition duration-300 z-50" id="popUp" style="display: none;">
-        <div class="flex items-center m-auto justify-center px-6 py-3 mb-4 text-sm text-red-800 rounded-lg bg-red-50 dark:bg-gray-800 dark:text-red-400">
-            <svg class="flex-shrink-0 inline w-4 h-4 me-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-                <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z"/>
-            </svg>
-            <span class="sr-only">Info</span>
-            <div id="errorMessage"></div>
-        </div>
-    </div>
-
-
-    <!-- scriopt -->
-    <script src="validation.js"></script>
-
-    <!-- JavaScript function -->
-    <script>
-        function displayErrorMessage(message) {
-            let popUp = document.getElementById('popUp');
-            let errorMessage = document.getElementById('errorMessage');
-
-            errorMessage.innerHTML = '<span class="font-medium">' + message + '</span>';
-            popUp.style.display = 'flex';
-            popUp.style.opacity = '100';
-
-            setTimeout(() => {
-                popUp.style.display = 'none';
-                popUp.style.opacity = '0';
-            }, 1500);
-        }
-    </script>
-
 </body>
+
 </html>
-
-
-<?php
-    include "../../database/connect.php";
-    
-
-    if(isset($_POST['regBtn'])){
-        $file_name = $_FILES['UserImage']['name'];
-        $tempname = $_FILES['UserImage']['tmp_name'];
-        $folder = '../../src/user_dp/'.$file_name;
-        $fname = $_POST['fname'];
-        $lname = $_POST['lname'];
-        $email = $_POST['email'];
-        $password = $_POST['password'];
-        $address = $_POST['address'];
-        $mobileno = $_POST['mobileno'];
-        $state = $_POST['state'];
-        $city = $_POST['city'];
-        $pincode = $_POST['pincode'];
-        $user_reg_date = date('d-m-Y');;
-
-        // preg_match
-        $firstname_pattern = "/^[a-zA-Z]([0-9a-zA-Z]){2,10}$/";
-        $lastname_pattern = "/^[a-zA-Z]([0-9a-zA-Z]){2,10}$/";
-        $email_pattern = "/^[a-zA-Z][a-zA-Z0-9._-]*@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/";
-        $password_pattern = '/^.{8,}$/';
-        $address_pattern = "/^[\w\d\s#.,'-]{5,}$/i";
-        $number_pattern = "/^[0-9]{10}$/";
-        $state_pattern = "/^[a-zA-Z\s'-]+$/";
-        $city_pattern = "/^[a-zA-Z\s'-]+$/";
-        $pincode_pattern = "/^[1-9][0-9]{5}$/";
-
-        // Validation for first name
-        if (!preg_match($firstname_pattern, $fname)) {
-            echo '<script>displayErrorMessage("First name must be 2-10 character long and shuld not start with a number.");</script>';
-        }
-        // Validation for last name
-        if (!preg_match($lastname_pattern, $lname)) {
-            echo '<script>displayErrorMessage("Last name must be 2-10 character long and shuld not start with a number.");</script>';
-        }
-        // Validation for email
-        if (!preg_match($email_pattern, $email)) {
-            echo '<script>displayErrorMessage("Invalid Email.");</script>';
-        }
-        // Validation for password
-        if (!preg_match($password_pattern, $password)) {
-            echo '<script>displayErrorMessage("Invalid Password.");</script>';
-        }
-        // Validation for adddress
-        if (!preg_match($address_pattern, $address)) {
-            echo '<script>displayErrorMessage("Invalid Address.");</script>';
-        }
-        // Validation for phone number
-        if (!preg_match($number_pattern, $mobileno)) {
-            echo '<script>displayErrorMessage("Invalid Phone Number.");</script>';
-        }
-        // Validation for state
-        if (!preg_match($state_pattern, $state)) {
-            echo '<script>displayErrorMessage("Invalid State.");</script>';
-        }
-        // Validation for City
-        if (!preg_match($city_pattern, $city)) {
-            echo '<script>displayErrorMessage("Invalid City.");</script>';
-        }
-        // Validation for pincode
-        if (!preg_match($pincode_pattern, $pincode)) {
-            echo '<script>displayErrorMessage("Invalid Pincode.");</script>';
-        }
-
-
-        // hash pass
-        $pass = password_hash($password, PASSWORD_BCRYPT);
-
-        $email_check = "SELECT * FROM user_registration WHERE email = '$email'";
-        $check_query = mysqli_query($con, $email_check);
-
-        $emailCount = mysqli_num_rows($check_query);
-
-        if($emailCount > 0){
-            ?>
-                <!-- Error -->
-                <div class="validInfo fixed top-0 mt-2 w-full transition duration-300 z-50" id="popUp" style="display: none;">
-                    <div class="flex items-center m-auto justify-center px-6 py-3 mb-4 text-sm text-red-800 rounded-lg bg-red-50 dark:bg-gray-800 dark:text-red-400" role="alert">
-                        <svg class="flex-shrink-0 inline w-4 h-4 me-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20"><path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z"/></svg>
-                        <span class="sr-only">Info</span>
-                        <div>
-                            <span class="font-medium">Email already Exists.</span>
-                        </div>
-                    </div>
-                </div>
-
-                <script>
-                
-                    let popUp = document.getElementById('popUp');
-
-                    popUp.style.display = 'flex';
-                    popUp.style.opacity = '100';
-
-                    setTimeout(() => {
-                        popUp.style.display = 'none';
-                        popUp.style.opacity = '0';
-                    }, 1500);
-                </script>
-            <?php
-        }else if(move_uploaded_file($tempname, $folder)){
-            $insert_reg_data = "INSERT INTO user_registration(first_name, last_name, phone, email, profile_image, Address, state, city, pin, password, date) VALUES ('$fname','$lname','$mobileno','$email','$file_name','$address','$state','$city','$pincode','$pass','$user_reg_date')";
-            $iquery = mysqli_query($con,$insert_reg_data);
-
-            if($iquery){
-                ?>
-                    <!-- Successfully -->
-                    <div class="validInfo fixed top-0 mt-2 w-full transition duration-300 z-50" id="popUp" style="display: none;">
-                        <div class="flex items-center m-auto justify-center px-6 py-3 mb-4 text-sm text-green-800 rounded-lg bg-green-50 dark:bg-gray-800 dark:text-green-400" role="alert">
-                            <svg class="flex-shrink-0 inline w-4 h-4 me-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20"><path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z"/></svg>
-                            <span class="sr-only">Info</span>
-                            <div>
-                                <span class="font-medium">Inserted successful.</span>
-                            </div>
-                        </div>
-                    </div>
-
-                    <script>
-                    
-                        let popUp = document.getElementById('popUp');
-
-                        popUp.style.display = 'flex';
-                        popUp.style.opacity = '100';
-
-                        setTimeout(() => {
-                            popUp.style.display = 'none';
-                            popUp.style.opacity = '0';
-                        }, 1500);
-                    </script>
-                    <script>window.location.href = "user_login.php";</script>
-                <?php
-                
-                exit;
-            }else{
-                ?>
-                    <!-- Error -->
-                    <div class="validInfo fixed top-0 mt-2 w-full transition duration-300 z-50" id="popUp" style="display: none;">
-                        <div class="flex items-center m-auto justify-center px-6 py-3 mb-4 text-sm text-red-800 rounded-lg bg-red-50 dark:bg-gray-800 dark:text-red-400" role="alert">
-                            <svg class="flex-shrink-0 inline w-4 h-4 me-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20"><path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z"/></svg>
-                            <span class="sr-only">Info</span>
-                            <div>
-                                <span class="font-medium">Insertion Failed.</span>
-                            </div>
-                        </div>
-                    </div>
-
-                    <script>
-                    
-                        let popUp = document.getElementById('popUp');
-
-                        popUp.style.display = 'flex';
-                        popUp.style.opacity = '100';
-
-                        setTimeout(() => {
-                            popUp.style.display = 'none';
-                            popUp.style.opacity = '0';
-                        }, 1500);
-                    </script>
-                <?php
-            }
-        }
-
-    }
-?>
