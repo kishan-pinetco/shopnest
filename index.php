92bdd9baf88bb17922814cd8a4fd28ff9680cfa5
vishvjit2210 <148254408+vishvjit2210@users.noreply.github.com>
2024-07-29 16:32:10 +0530

    update_index_page

--- index.php
+++ index.php
@@ -110,7 +110,7 @@ Beauty & Heathy -->
                 </span>
             </button>
             <button type="button" class="absolute top-0 end-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-next>
-                <span class="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white bg-white hover:ring-2 hover:ring-indigo-500">
+                <span class="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white hover:ring-2 hover:ring-indigo-500">
                     <svg class="w-4 h-4 text-black rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
                         <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 9 4-4-4-4" />
                     </svg>
