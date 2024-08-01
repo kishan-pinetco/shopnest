a63cbacbf11524349da40966f6c5f2141d3c12cd
vishvjit2210 <148254408+vishvjit2210@users.noreply.github.com>
2024-08-01 17:00:41 +0530

    add_card_slider_update_navbar

--- pages/_navbar.php
+++ pages/_navbar.php
@@ -285,7 +285,19 @@ if (isset($_COOKIE['user_id'])) {
                                 <path d="M164.96 300.004h.024c.02 0 .04-.004.059-.004H437a15.003 15.003 0 0 0 14.422-10.879l60-210a15.003 15.003 0 0 0-2.445-13.152A15.006 15.006 0 0 0 497 60H130.367l-10.722-48.254A15.003 15.003 0 0 0 105 0H15C6.715 0 0 6.715 0 15s6.715 15 15 15h77.969c1.898 8.55 51.312 230.918 54.156 243.71C131.184 280.64 120 296.536 120 315c0 24.812 20.188 45 45 45h272c8.285 0 15-6.715 15-15s-6.715-15-15-15H165c-8.27 0-15-6.73-15-15 0-8.258 6.707-14.977 14.96-14.996zM477.114 90l-51.43 180H177.032l-40-180zM150 405c0 24.813 20.188 45 45 45s45-20.188 45-45-20.188-45-45-45-45 20.188-45 45zm45-15c8.27 0 15 6.73 15 15s-6.73 15-15 15-15-6.73-15-15 6.73-15 15-15zM362 405c0 24.813 20.188 45 45 45s45-20.188 45-45-20.188-45-45-45-45 20.188-45 45zm45-15c8.27 0 15 6.73 15 15s-6.73 15-15 15-15-6.73-15-15 6.73-15 15-15zm0 0" fill="#ffffff" opacity="1" data-original="#000000"></path>
                             </g>
                         </svg>
-                        <p class="text-white text-center rounded-full text-xs px-1 absolute -top-2 -right-2 bg-[#ff0000]">0</p>
+                        <p class="text-white text-center rounded-full text-xs px-1 absolute -top-2 -right-2 bg-[#ff0000]">
+                            <?php
+                            if (isset($_COOKIE['Cart_products'])) {
+                                $cookie_value = $_COOKIE['Cart_products'];
+
+                                $cart_products = json_decode($cookie_value, true);
+                                if (!empty($cart_products) && is_array($cart_products)) {
+                                    $totalCartItems = count($cart_products);
+                                    echo $totalCartItems;
+                                }
+                            }
+                            ?>
+                        </p>
                     </a>
                 </div>
                 <div>
