41409b7710c8ad3c4426e56897ef512c3167d883
vishvjit2210 <148254408+vishvjit2210@users.noreply.github.com>
2024-09-16 16:44:58 +0530

    add chatBoat

--- authentication/forgotPassword.php
+++ authentication/forgotPassword.php
@@ -245,6 +245,9 @@
             adjustContainerHeight();
         }
     </script>
+
+    <!-- chatboat script -->
+    <script type="text/javascript" id="hs-script-loader" async defer src="//js-na1.hs-scripts.com/47227404.js"></script>
 </body>
 
 </html>
